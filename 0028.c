479b3af437ae6d83aacc3ac9a17d62a53f9a6964
pagd101221hdfsvva8-ui <pagd101221hdfsvva8@lake.edu.mx>
2025-11-14 23:50:03 -0600

    Update and rename 0028.c to cuadrado.c

--- 0028.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-main(){
-int Resultado;
-Resultado=243*243;
-printf("El cuadrado de 243 es: %d", Resultado);
-}
---
+++ cuadrado.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+   Para calcular el cuadrado de un número basta con multiplicarlo por sí mismo.
+   En este caso se realiza: 243 * 243.
+*/
+
+main(){
+    int Resultado;
+    Resultado = 243 * 243;
+    printf("El cuadrado de 243 es: %d", Resultado);
+}
