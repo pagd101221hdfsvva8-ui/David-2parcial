0077f2aca4676e33d53601c7b20e55cd95657f9a
pagd101221hdfsvva8-ui <pagd101221hdfsvva8@lake.edu.mx>
2025-11-14 23:49:29 -0600

    Update and rename 0028 (1).c to suma de tres numeros.c

---
+++ suma de tres numeros.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+   Para obtener el cuadrado de 243 simplemente se multiplica
+   el número por sí mismo: 243 * 243.
+*/
+
+main(){
+    int Resultado;
+    Resultado = 243 * 243;
+    printf("El cuadrado de 243 es: %d", Resultado);
+}
